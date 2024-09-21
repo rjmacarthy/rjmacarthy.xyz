7403278c65aa8c7e2a93e45a0e44b985da6d3291
rjmacarthy <richardmacarthy@protonmail.com>
2024-09-21 13:29:19 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const http = require("http");
+const pug = require("pug");
+const fs = require("fs");
+const path = require("path");
 
-const PORT = 3001
+const PORT = 3001;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("rjmacarthy.xyz");
+  const templatePath = path.join(__dirname, "index.pug");
+  
+  fs.readFile(templatePath, "utf8", (err, pugTemplate) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+      return;
+    }
+
+    try {
+      const compiledFunction = pug.compile(pugTemplate);
+      const renderedHtml = compiledFunction();
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(renderedHtml);
+    } catch (error) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error rendering template");
+    }
+  });
 });
 
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
